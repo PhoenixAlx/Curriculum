8e0f8959b6522f190800aff3e85dc46b8233204c
PhoenixAlx <raskalakabra@gmail.com>
2017-04-24 15:26:38 +0100

    reconfigure all, new format

--- js/cvdatas.js
+++ js/cvdatas.js
@@ -1,4 +1,6 @@
-
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
 class datasCV{
     constructor(){
         this.datas = JSON.parse(data);
@@ -7,11 +9,41 @@ class datasCV{
         var cate_list=[];
         for (var i=0;i< this.datas.length;i++){
             var cate=this.datas[i].category;
-            if ((cate_list.indexOf(cate)<0) && (cate == this.datas[i].field)){
-                cate_list.push(this.datas[i].text_field)
+
+            if ((cate_list.indexOf(cate)<0) ){
+                cate_list.push(this.datas[i].category)
             }
         }
+
         return cate_list;
     }
-    
+    value(field){
+        var valor="";
+        for (var i=0;i< this.datas.length;i++){
+            var name_field=this.datas[i].field;
+            if (name_field == field){
+                valor = this.datas[i].info;
+                return valor;
+            }
+        }
+    }
+    all_values(field){
+        var value_list=[];
+        for (var i=0;i< this.datas.length;i++){
+            var valor = this.datas[i];
+            value_list.push(valor[field]);
+        }
+
+        return value_list;
+    }
+    all_values_uni(field){
+        var value_list=[];
+        for (var i=0;i< this.datas.length;i++){
+            var valor = this.datas[i];
+            value_list.push(valor[field]);
+        }
+        var unique = Array.from(new Set( value_list )); 
+
+        return unique;
+    }
 }
--- js/cvformat.js
+++ js/cvformat.js
@@ -1,36 +1,70 @@
 window.onload= loadCVFull;
 
-function loadHeader(categ){
+function loadHeader(nameTitle){
+    
+  
+    var header='<div class="container_16"><hgroup><h1>'+nameTitle+'</h1></hgroup><figure>		<img src="img/perfil.jpg" alt="'+nameTitle+'"></figure></div>'
+    
+    
+    
+    $("#header").append(header);
+}
+function loadLateralMenu(categ){
     
     var list_categ="";
     for (var i=0;i<categ.length;i++){
-        list_categ=list_categ+'<li class="uk-nav-divider"><a href="#">'+categ[i]+'</a></li>';
+        list_categ=list_categ+'<li class="pushy-link"><a href="#">'+categ[i]+'</a></li>';
     }
-    var menu='<ul class="uk-navbar-nav"><li class="uk-active"><a href="#" ><span >Completo<span></a></li><li ><a href="#" >Categorías</a><div class="uk-navbar-dropdown"><ul class="uk-nav uk-navbar-dropdown-nav">'+list_categ+'</ul></div></li></ul>';
-    var header='<header ><h1 class="uk-heading-primary uk-position-small uk-position-relative uk-position-top-center ">CURRÍCULUM</h1><nav class="uk-navbar-container" uk-navbar>'+menu+'</nav></header><hr class="uk-divider-icon" />';
-    $("body").prepend(header);
+    
+    
+    $("#submenuCat").append(list_categ);
+}
+function loadInformation(cv_datas){
+    //load sections
+    var sections='<section role="main" class="container_16 " >';
+    var cate=cv_datas.all_values_uni("id_category");
+    var name_cate=cv_datas.all_values_uni("category");
+    for (var c=0;c< cate.length;c++){
+        var ca=cate[c];
+        var section='<div class="grid_16"><div class="grid_16  '+ca+'""><h3>'+name_cate[c]+'</h3>';
+        
+        for (var i=0;i< cv_datas.datas.length;i++){
+            
+            if (ca==cv_datas.datas[i].id_category){
+                if (cv_datas.datas[i].field!="photoCV"){
+                    section=section+'<p><b>'+cv_datas.datas[i].text_field+':</b> <span>'+cv_datas.datas[i].info+'</span></p>';
+                }
+            }
+        }
+        section=section+'</div></div>';
+        
+        sections=sections+section;
+    }
+    sections=sections+'</section>';
+    $("#container").append(sections);
 }
 function createTreeCategories(cv_datas){
     var section_categories='';
     var categories=[];
     //for in datas
-    console.log(cv_datas)
     for (var i=0;i< cv_datas.length;i++){
             var cate=cv_datas[i].category;
-            console.log(cate)
             if (cate == cv_datas[i].field){
                 categories.push([cv_datas[i].text_field,cv_datas[i].info]);
                 section_categories=section_categories+'<section class="uk-section uk-section-primary  uk-column-divider  "><div class="uk-grid-match uk-child-width-1-2@m" uk-grid><h3 >'+cv_datas[i].text_field+'</h3><h2 >'+cv_datas[i].info+'</h2></div></section>'
             }
     }
-    console.log(section_categories)
+
     $("body").append(section_categories);
 }
 function loadCVFull(){
     //Load full Curriculum
     var datas_cv= new datasCV();
     // make header
-    var cate=datas_cv.name_categories;
-    loadHeader(cate);
+    var cate=datas_cv.all_values_uni("category");
+    var nameTitle=datas_cv.value("name");
+    loadHeader(nameTitle);
+    loadLateralMenu(cate);
+    loadInformation(datas_cv);
     createTreeCategories(datas_cv.datas);
 } 
