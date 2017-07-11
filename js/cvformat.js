af4958598ef6b6c99b9785aa9b5d44292a7f8b3b
PhoenixAlx <raskalakabra@gmail.com>
2017-07-11 16:21:57 +0100

    add new pdf export

--- js/cvformat.js
+++ js/cvformat.js
@@ -252,7 +252,7 @@ class CV{
             if (hostname ==""){
               this.createPDF("");
             }else{
-              this.getDataUri('/img/perfil.jpg', function(dataUri) {
+              this.getDataUri('Curriculum/img/perfil.jpg', function(dataUri) {
                 this.createPDF(dataUri);
               });
             }
